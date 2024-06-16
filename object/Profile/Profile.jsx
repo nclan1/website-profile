226f19bc765c47065acf3f33a21a48248674a3b9
nclan1 <nclan@bu.edu>
2024-06-15 20:45:31 -0400

    OMFG parallax effect yay!

--- object/Profile/Profile.jsx
+++ object/Profile/Profile.jsx
@@ -1,22 +1,41 @@
-
+'use client';
 
 import styles from "./style.module.scss";
 import Image from "next/image";
 import profilePic from "./Profile.jpg";
+import { useRef } from "react";
+import { useScroll, useTransform, motion } from "framer-motion";
 
 export default function Profile(){
-    return (
-        <div className={styles.headline}>
-            <Image 
-            className={styles.profilePic}
-            src={profilePic}
-            alt='nolan ngim'
-            />
 
-            <div className={styles.school}>
-                <h1>boston <a>uni</a></h1>
-                <h2>42°21’28’’N, 71°03’42”O</h2>
-            </div>
+    const container = useRef(null);
+    const {scrollYProgress} = useScroll({
+        target: container,
+        offset: ['start end', 'end start']
+    })
+
+    const sm = useTransform(scrollYProgress, [0, 1], [0, -50]);
+    const md = useTransform(scrollYProgress, [0, 1], [0, -150]);
+    const lg = useTransform(scrollYProgress, [0, 1], [0, -250]);
+    
+    return (
+        <div className={styles.headline} ref={container}>
+            <motion.h1 style={{y: lg}}>
+                <Image 
+                className={styles.profilePic}
+                src={profilePic}
+                alt='nolan ngim'
+                />
+            </motion.h1>
+            
+
+            <motion.h1 style={{y: md}}>
+                <div className={styles.school}>
+                    <h1>boston <a>uni</a></h1>
+                    <h2>42°21’28’’N, 71°03’42”O</h2>
+                </div>
+            </motion.h1>
+            
         </div>
         
     )
