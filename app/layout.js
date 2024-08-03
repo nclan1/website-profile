a6598fc6bda5f18549baed44b49f41a1fa18e509
nclan1 <nclan@bu.edu>
2024-08-03 14:12:10 -0400

    added analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import localFont from 'next/font/local';
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 const myFont = localFont({
   src: "../assets/fonts/GeneralSans-Variable.ttf",
@@ -15,7 +16,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={myFont.className}>{children}</body>
+      <body className={myFont.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
